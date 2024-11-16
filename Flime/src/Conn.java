7ac9a11d40de3f4c78593fc82f327583f187ebc0
Raghad b <58854225+rasbrc@users.noreply.github.com>
2024-11-16 15:42:30 +0300

    Revert ".."
    This reverts commit cdab1315c1006eaf16d05bb199b4e76a3b5b1122.

--- Flime/src/Conn.java
+++ Flime/src/Conn.java
@@ -23,43 +23,6 @@ public class Conn {
             Logger.getLogger(Conn.class.getName()).log(Level.SEVERE, null, ex);
         }
     
-        
-      try {
-            if(USER.equals("cashier")){
-            Class.forName("com.mysql.cj.jdbc.Driver");
-            Connection conn = DriverManager.getConnection(DB_URL, USER, PASS);
-            Statement ss = conn.createStatement();
-            ResultSet rs = ss.executeQuery("SELECT C.cashier_id, C.phone_number, C.first_name,C.last_name,C.salary FROM cashier C WHERE cashier_id=ID");
-           while(rs.next()){
-                 jTextField1.setText(rs.getString("cashier_id")) ;
-                 jTextField2.setText(rs.getString("first_name")) ;
-                 jTextField3.setText(rs.getString("last_name")) ;
-                 jTextField4.setText(rs.getString("phone_number")) ;
-                 jTextField5.setText(rs.getString("salary")) ;
-           }}
-           
-            else{
-            Class.forName("com.mysql.cj.jdbc.Driver");
-            Connection conn = DriverManager.getConnection(DB_URL, USER, PASS);
-            Statement ss = conn.createStatement();
-            ResultSet rs = ss.executeQuery("SELECT P.preparer_id, P.phone_number, P.first_name,P.last_name,P.salary FROM food_preparer P WHERE preparer_id=ID");
-           while(rs.next()){
-                 jTextField1.setText(rs.getString("preparer_id")) ;
-                 jTextField2.setText(rs.getString("first_name")) ;
-                 jTextField3.setText(rs.getString("last_name")) ;
-                 jTextField4.setText(rs.getString("phone_number")) ;
-                 jTextField5.setText(rs.getString("salary")) ;
-            }}
-        } catch (SQLException ex) {
-            Logger.getLogger(Conn.class.getName()).log(Level.SEVERE, null, ex);
-        } catch (ClassNotFoundException ex) {
-            Logger.getLogger(Conn.class.getName()).log(Level.SEVERE, null, ex);
-        }
-    
-    
-
-      
-        
     }
 
     }
